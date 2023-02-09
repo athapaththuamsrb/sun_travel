63c0744f98c28e4d2c7a918b6107f22176032d3a
rukshan <rukshan@codegen.net>
2023-02-10 01:58:21 +0530

    add kubanates to this

--- frontend/sun-Travel/environment.ts
+++ frontend/sun-Travel/environment.ts
@@ -1,4 +1,10 @@
 export const environment = {
     production: false,
-    baseUrl: "http://localhost:8080/api/v1/"
+    /* baseUrl: "http://localhost:4200/api/v1/" ->
+    if add folowing part to bginx.conf
+    location /api/v1
+        # {
+        #     proxy_pass http://localhost:8080/api/v1;
+        # }*/
+    baseUrl: "http://192.168.49.2:30163/api/v1"
 }
